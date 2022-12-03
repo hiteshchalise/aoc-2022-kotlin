3cd6ab8a98512120d0bac6a82cbb7b1042a11ef7
Hitesh Chalise <hiteshchalise10@gmail.com>
2022-12-03 10:08:11 +0545

    Day 1 completed.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,33 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val caloriesList = input.map { if (it.isNotEmpty()) it.toInt() else 0 }
+        val listOfTotalCalories = mutableListOf<Int>()
+        caloriesList.reduce { acc: Int, calories: Int ->
+            if (calories > 0) acc + calories
+            else {
+                listOfTotalCalories.add(acc)
+                0
+            }
+        }
+        return listOfTotalCalories.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val caloriesList = input.map { if (it.isNotEmpty()) it.toInt() else 0 }
+        val listOfTotalCalories = mutableListOf<Int>()
+        caloriesList.reduce { acc: Int, calories: Int ->
+            if (calories > 0) acc + calories
+            else {
+                listOfTotalCalories.add(acc)
+                0
+            }
+        }
+        return listOfTotalCalories.sortedDescending().subList(0, 3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
